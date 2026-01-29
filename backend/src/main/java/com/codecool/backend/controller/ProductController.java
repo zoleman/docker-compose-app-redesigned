f3b980371463b203e971770f49cf9f5a68646051
PeterKokenyessy <kokenypeter2001@gmail.com>
2026-01-29 07:58:09 +0100

    - create vite and react and connect to backend - bacend add env to gitignor and controller CrossOrigin to connect forntend

--- backend/src/main/java/com/codecool/backend/controller/ProductController.java
+++ backend/src/main/java/com/codecool/backend/controller/ProductController.java
@@ -1,16 +1,19 @@
 package com.codecool.backend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/products")
 public class ProductController {
     @GetMapping
     public Map<String, String> getAllProducts() {
+        System.out.println("hivas");
         return Map.of("status", "ok");
     }
 }
